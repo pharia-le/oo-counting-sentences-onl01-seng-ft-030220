b69d3ae49bd7d1f50f4b78aeab80f3ee1087c615
Pharia Le <pharia.le@gmail.com>
2020-03-11 22:55:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
+   def count_sentences
+    return 0 if self.length == 0 
+    
+    count = 0
+    i = 0
+    marks = [".", "?", "!"]
 
+    while i < self.length-1
+      if marks.include?(self[i]) && (marks.include?(self[i]) != marks.include?(self[i+1]))
+        count+=1
+      end
+      i+=1
+    end
+    count + 1
   end
+  
 end
